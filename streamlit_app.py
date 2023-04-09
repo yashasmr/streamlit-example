b81bfa0828e1e3d1fbfe3823afcb4c7ce442004f
yashasmr <86516855+yashasmr@users.noreply.github.com>
2023-04-09 15:23:18 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,20 @@ for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
+
+def main():
+    st.title(" Prediction")
+    
+    menu = ["Home","Dataset"]
+    choice = st.sidebar.selectboc("Menu",menu)
+    
+    if choice == "Home":
+        st.subheader("Home")
+        
+    elif choice == "Dataset":
+        st.subheader("Dataset")
+        
+    return
+    
+if __name__ == '__main__':
+    main()
