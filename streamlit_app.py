9b228497d46f63c3cee632fadf07ad3f7ef3799e
yashasmr <86516855+yashasmr@users.noreply.github.com>
2023-04-09 14:43:29 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,15 +33,15 @@ def main():
 if __name__ == '__main__':
     choice == main()
     
-if choice == "Home":
-    """ asghgferghgfdsdfbdfg """
-elif choice == "Dataset":
-    st.subheader("Dataset")
-    data_file = st.file_uploader(" Upload CSV ", type=["csv"])
-    if data_file is not None:
-        file_details = {"filename":data_file.name, "filetype":data_file.type,
-                        "filesize":data_file.size}
-        st.write(file_details)
-        predict_app = pd.read_csv(data_file)
-        st.dataframe(predict_app)
-        Input_data = data_eda(predict_app)
+# if choice == "Home":
+#     """ asghgferghgfdsdfbdfg """
+# elif choice == "Dataset":
+#     st.subheader("Dataset")
+#     data_file = st.file_uploader(" Upload CSV ", type=["csv"])
+#     if data_file is not None:
+#         file_details = {"filename":data_file.name, "filetype":data_file.type,
+#                         "filesize":data_file.size}
+#         st.write(file_details)
+#         predict_app = pd.read_csv(data_file)
+#         st.dataframe(predict_app)
+#         Input_data = data_eda(predict_app)
