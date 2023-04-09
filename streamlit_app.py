3d9b0ae8b635f5dd6cb06274496a6fdb84b4b081
yashasmr <86516855+yashasmr@users.noreply.github.com>
2023-04-09 14:40:29 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,23 +16,32 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def main():
+    st.title(" Prediction")
+    
+    menu = ["Home","Dataset"]
+    choice = st.sidebar.selectboc("Menu",menu)
+    
+    if choice == "Home":
+        st.subheader("Home")
+        
+    elif choice == "Dataset":
+        st.subheader("Dataset")
+        
+    return
+    
+if __name__ == '__main__':
+    choice == main()
+    
+if choice == "Home":
+    """ asghgferghgfdsdfbdfg """
+elif choice == "Dataset"
+    st.subheader("Dataset")
+    data_file = st.file_uploader(" Upload CSV ", type=["csv"])
+    if data_file is not None:
+        file_details = {"filename":data_file.name, "filetype":data_file.type,
+                        "filesize":data_file.size}
+        st.write(file_details)
+        predict_app = pd.read_csv(data_file)
+        st.dataframe(predict_app)
+        Input_data = data_eda(predict_app)
